2e9190da49a8fe5afc1ef2d1da0f9823a9d7e13f
Matias Borghi <borghi.matias@gmail.com>
2021-12-10 11:03:15 -0300

    minor addition

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-	export let name: string;
+	// export let name: string;
+	let name2: string;
+
+	function setName(name: Event): void {
+		console.log(name)
+		// console.log("given name2: " + name2, "set name2 to: " + namen);
+		// name2 = namen;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<label for="name">Name:</label><input id="name" type="text" value={setName}>
+	<h1>Hello {name2}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
